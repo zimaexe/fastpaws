307649261f324b259e908b7cf90f5ac01b7ce126
krrysanova <krisanovapolina22@gmail.com>
2025-01-10 19:14:20 +0100

    first page of deskop

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,30 +4,36 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState('');
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  };
+  const handleClick = () => {
+    alert('Кнопка натиснута!');
+  };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="white_field">
+        <div className="text_part_of_white_field">
+          <h1>Hello, I’m <span style={{ color: '#0096C7' }}>FastPaws</span></h1>
+          <p className='bot-description'>I’m here to help you quickly access your information! Enter a name and your question, and I’ll fetch the details about policies, claims, or medication coverage for you in no time.</p>
+        </div>
+        <div className='prompt'><label>
+          <input
+            className='prompt-input'
+            type="text"
+            value={text}
+            onChange={handleChange}
+            placeholder="tell me about you"
+          />
+        </label>
+          <button className="image-button" onClick={handleClick}>
+            <img className='image-of-bottom' src="./images/bottom.png" alt="Icon" />
+          </button>
+        </div>
+      </div >
+      <img className='doctor-cat' src="./images/doctor-cat.png" alt="Icon" />
     </>
   )
 }
