58ea40067c71d05ad1c3949554855c9be71593c4
Vadym Tovtyn <78219653+vadim-ghostman@users.noreply.github.com>
2025-01-15 11:29:23 +0100

    HOTFIX ORIGINS SET CORS

--- backend/main.py
+++ backend/main.py
@@ -40,9 +40,12 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
